f0d0f7d22870059bcb514fb94c382e3c467e372e
Maria Bucura <ronnie_ssj4@yahoo.com>
2021-10-03 22:13:25 +0300

    implementat cerinta 13 + creat functia main

--- main.py
+++ main.py
@@ -1,33 +1,54 @@
 '''
-Returneaza true daca n este prim si false daca nu.
+Transformarea unei temperaturi dintr-o scara data intr-o temperatura din alta scara data
 '''
-def is_prime(n):
-  # codul vostru aici
-  
-  
+def get_temp(temp, frm, to):
+    if frm == 'C' and to == 'F':
+        conversion = 9 / 5 * temp + 32
+        conversion = round(conversion, 3)
+    elif frm == 'K' and to == 'F':
+        conversion = (temp - 273.15) * 9/5 + 32
+        conversion = round(conversion, 3)
+    elif frm == 'F' and to == 'C':
+        conversion = 5 / 9 * (temp - 32)
+        conversion = round(conversion, 3)
+    elif frm == 'C' and to == 'K':
+        conversion = temp + 273.15
+        conversion = round(conversion, 3)
+    elif frm == 'K' and to == 'C':
+        conversion  = temp - 273.15
+        conversion = round(conversion, 3)
+    else:
+        conversion = 5 / 9 * (temp - 32) + 273.15
+        conversion = round(conversion, 3)
+    return conversion
 '''
-Returneaza produsul numerelor din lista lst.
+Functia test pentru functia get_temp
 '''
-def get_product(lst):
-  # codul vostru aici
-  
-  
-'''
-Returneaza CMMDC a doua numere x si y folosind primul algoritm.
-'''
-def get_cmmdc_v1(x, y):
-  # codul vostru aici
-  
-  
-'''
-Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
-'''
-def get_cmmdc_v2(x, y):
-  # codul vostru aici
-  
-  
-def main():
-  # interfata de tip consola aici
+def test_get_temp():
+    assert get_temp(100, 'C', 'F') == 212
+    assert get_temp(345, 'K', 'F') == 161.33
+    assert get_temp(113, 'F', 'C') == 45
+    assert get_temp(113, 'C', 'K') == 386.15
+    assert get_temp(113, 'K', 'C') == -160.15
+    assert get_temp(123, 'F', 'K') == 323.706
+    assert get_temp(35, 'C', 'F') == 95
 
-if __name__ == '__main__':
-  main()
+def main():
+    while True:
+        print('1. Transformarea unei temperaturi date intr-o alta temperatura.')
+        print('2. Calcularea celui mai mic multiplu comun la n numere date.')
+        print('x. Iesire din program')
+        option = input('Alege optiunea: ')
+        if option == '1':
+            temp = float(input('Introduceti temperatura: '))
+            frm = input('Din: ')
+            to = input('In: ')
+            c = get_temp(temp, frm, to)
+            print(f'{temp} {frm} --> {c} {to}')
+        elif option == 'x':
+            break
+        else:
+            print('Optiune Invalida!')
+
+test_get_temp()
+main()
